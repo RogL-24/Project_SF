f7aea4d4481c9e284e423edf42b4482e8846f6d1
RogL-24 <rogersamuellokku@outlook.com>
2025-05-29 09:53:10 +0200

    Task 3 and 4
    Solution to aboce mentioned tasks have been added.

---
+++ tu_qw.m
@@ -0,0 +1,18 @@
+function [x, P] = tu_qw(x, P, omega, T, Rw)
+    % checks if omega is available.
+    n = length(omega);
+    if n > 0 & ~isnan(omega) 
+        % F = I + AT; where A = 1/2 * S(\omega);
+        F = eye(length(x)) + 0.5 * Somega(omega) * T;
+    else
+        F = eye(length(x));
+    end
+
+    % G = 1/2 * S(q)*T
+    G = 0.5 * Sq(x) * T;
+
+    % State Prediction
+    x = F * x;
+    % Covariance Prediction.
+    P = F * P * F' + G * Rw * G';
+end
