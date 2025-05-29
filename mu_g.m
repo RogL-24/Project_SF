a7df76fb4d8fb84e1de9b667cf78ea6f127d8e44
RogL-24 <rogersamuellokku@outlook.com>
2025-05-29 13:07:30 +0200

    Task 6
    Changes to task 3 and new addition - Task 6

---
+++ mu_g.m
@@ -0,0 +1,21 @@
+function [x, P] = mu_g(x, P, yacc, Ra, g0)
+    
+    h = Qq(x)' * g0; % predicted measurement
+    
+    % Jacobian
+    [H1, H2, H3, H4] = dQqdq(x);
+    H(:,1) = H1' * g0;
+    H(:,2) = H2' * g0;
+    H(:,3) = H3' * g0;
+    H(:,4) = H4' * g0;
+
+    % Innovation gain
+    S = H * P * H' + Ra;
+    %Kalman Gain
+    K = P * H' * inv(S);
+    % state update 
+    x = x + K * (yacc - h);
+    % Covariance update
+    P = P - K * S * K';
+
+end
